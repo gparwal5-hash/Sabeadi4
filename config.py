28bf2b1b6fa2c0ddfbc8da40850462d72c1cf305
gparwal5-hash <gparwal5@gmail.com>
2026-09-24 01:56:34 +0530

    Update API credentials and admin ID in config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "24754824"))
+API_ID = int(os.environ.get("API_ID", "23878955"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "e24a9c7a6aa24e1c56fa349e104ec20e")
+API_HASH = os.environ.get("API_HASH", "07637a6c4c4566dc3923fa29d6c84b67")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "968292174"))
+ADMINS = int(os.environ.get("ADMINS", "5445688589"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
